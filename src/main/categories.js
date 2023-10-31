22f9a2bcecf523ddda308cc5fcd0e98f9a47370f
Rasheem07 <mohdrasheem@icloud.com>
2023-10-31 20:09:51 +0530

    changed category lists

--- src/main/categories.js
+++ src/main/categories.js
@@ -5,6 +5,23 @@ import "./categories.css"
 export function Category() {
   return (
     <>
+    <div className="card custom-card1">
+      <h2 className='my-0'>Essentials:</h2>
+      <div className='category-row'>
+        {categories_row1.map((category) => (
+          <div className='col-md-3 col-lg-2 col-sm-6' key={category.id}>
+            <div className='category-card'>
+              <img src={category.imageURL} alt={category.name} />
+              <p className='p-0 m-0'>{category.name}</p>
+              <p className="p-1 m-0 pricerange" >{category.priceRange}</p>
+                <p className="p-0 m-0 des" style={{ fontSize: "12px" }}>
+                  {category.description}
+                </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
     <div className="card custom-card1 p-0">
       <h2 className='my-0'>Essentials:</h2>
       <div className='category-row'>
@@ -13,45 +30,44 @@ export function Category() {
             <div className='category-card'>
               <img src={category.imageURL} alt={category.name} />
               <p className='p-0 m-0'>{category.name}</p>
+              <p className="p-1 m-0 pricerange" >{category.priceRange}</p>
+                <p className="p-0 m-0 des" style={{ fontSize: "10px" }}>
+                  {category.description}
+                </p>
             </div>
           </div>
         ))}
       </div>
       </div>
-    <div className="card custom-card1">
-      <h2>Essentials:</h2>
+    <div className="card custom-card1 p-0">
+      <h2 className='my-0'>Essentials:</h2>
       <div className='category-row'>
         {categories_row1.map((category) => (
-          <div className='col-md-3 col-lg-2 col-sm-4' key={category.id}>
-            <div className='category-card'>
-              <img src={category.imageURL} alt="error" />
-              <p>{category.name}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-      </div>
-    <div className="card custom-card1">
-      <h2>Essentials:</h2>
-      <div className='category-row'>
-        {categories_row1.map((category) => (
-          <div className='col-md-3 col-lg-2 col-sm-4' key={category.id}>
+          <div className='col-md-3 col-lg-2 col-sm-6' key={category.id}>
             <div className='category-card'>
               <img src={category.imageURL} alt={category.name} />
-              <p>{category.name}</p>
+              <p className='p-0 m-0'>{category.name}</p>
+              <p className="p-1 m-0 pricerange" >{category.priceRange}</p>
+                <p className="p-0 m-0 des" style={{ fontSize: "10px" }}>
+                  {category.description}
+                </p>
             </div>
           </div>
         ))}
       </div>
       </div>
-    <div className="card custom-card1">
-      <h2>Essentials:</h2>
+    <div className="card custom-card1 p-0">
+      <h2 className='my-0'>Essentials:</h2>
       <div className='category-row'>
         {categories_row1.map((category) => (
-          <div className='col-md-3 col-lg-2 col-sm-4' key={category.id}>
+          <div className='col-md-3 col-lg-2 col-sm-6' key={category.id}>
             <div className='category-card'>
               <img src={category.imageURL} alt={category.name} />
-              <p>{category.name}</p>
+              <p className='p-0 m-0'>{category.name}</p>
+              <p className="p-1 m-0 pricerange" >{category.priceRange}</p>
+                <p className="p-0 m-0 des" style={{ fontSize: "10px" }}>
+                  {category.description}
+                </p>
             </div>
           </div>
         ))}
